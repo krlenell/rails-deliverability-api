335e33b555e6259dbccc70722dd90fdd5a3d2367
Kevin Lenell <krlenell@gmail.com>
2020-12-13 21:20:16 -0800

    device inital migration

--- db/migrate/20201214051659_create_devices.rb
+++ db/migrate/20201214051659_create_devices.rb
@@ -1,8 +1,8 @@
 class CreateDevices < ActiveRecord::Migration[6.1]
   def change
     create_table :devices do |t|
-      t.text :phone_number null:false
-      t.text :carrier null:false
+      t.text :phone_number, null:false
+      t.text :carrier, null:false
       t.datetime :disabled_at, :default => nil
 
       t.timestamps
--- spec/factories/devices.rb
+++ spec/factories/devices.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :device do
     phone_number { "MyText" }
     carrier { "MyText" }
-    disabled_at { "2020-12-13 21:16:59" }
+    #disabled_at { "2020-12-13 21:16:59" }
+    disabled_at{ nil }
   end
 end
