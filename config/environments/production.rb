47e378446641bca4662e1f98c0a930177768332c
Kevin Lenell <krlenell@gmail.com>
2020-12-13 21:04:38 -0800

    re-re-recreating project basics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "arist_api_production"
+  # config.active_job.queue_name_prefix = "arist_deliverability_api_production"
 
   config.action_mailer.perform_caching = false
 
