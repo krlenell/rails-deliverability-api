cda073ba775642cb463a3fae486a918f747ec47d
Kevin Lenell <krlenell@gmail.com>
2020-12-16 01:53:15 -0800

    more controller tests and placeholders

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -38,6 +38,28 @@ RSpec.describe ApiController, type: :controller do
     # end
   end
 
+  describe "POST #report" do
+    context "with invalid params" do
+      it "fails when given nonexistant device_id" do
+        post :report, params: {device_id: "fake ID", sender: "1253-303-5074", message: "LOL 😁😁😁"}
+        expect(response).to have_http_status(500)
+      end
+      #do another with invalid params that aren't device ID
+    end
+    #context "with valid params" do
+      #still need to figure out this associated randomly generated UUID association test
+
+  end
+
+  describe "POST #terminate" do
+    context "with invalid params" do
+      it "fails when given nonexistant device_id" do
+        patch :terminate, params: {device_id: "fake ID"}
+        expect(response).to have_http_status(500)
+      end
+    end
+    #context "with valid params"
+  end
 
 
 end
